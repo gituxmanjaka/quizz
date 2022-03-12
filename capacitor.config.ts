7485a0814258567fa12ac81bfb165d3290c3b4d3
Toky Mahefa <toky@MacBook-Pro.local>
2022-03-12 20:00:53 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'quizz',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
